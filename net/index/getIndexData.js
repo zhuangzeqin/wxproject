5582ff7a02c0a821ce7ee70b6f0c9bcbfb2da9ed
zhuangzeqin <zzq@eeepay.cn>
2020-10-29 17:27:27 +0800

    test  一下
    测试一下

--- net/index/getIndexData.js
+++ net/index/getIndexData.js
@@ -8,6 +8,7 @@ export const getBannerData = params => reqeust({url: '/banners', data: params});
 //   // })
 // }
 export const getGanHuo  = params => reqeust({url: '/data/category/GanHuo/type/Android/page/'+params[0]+'/count/'+params[1]});
+//
 // {
 //   var urlStr = "/data/category/GanHuo/type/Android/page/0/count/10"
 //   console.log("urlStr="+urlStr)
